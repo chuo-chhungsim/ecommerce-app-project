0a4d16dd2b8f7212908eb21eb6fbb3ae754d1926
chuo-chhungsim <chhungsim.chuo@gmail.com>
2025-08-29 16:06:54 +0700

    init project

---
+++ app/src/main/java/com/example/ecommere_app/ui/theme/Type.kt
@@ -0,0 +1,39 @@
+package com.example.ecommere_app.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.ecommere_app.R
+
+// Set of Material typography styles to start with
+val Outfit = FontFamily(
+    Font(R.font.outfit_regular, FontWeight.Normal),
+    Font(R.font.outfit_medium, FontWeight.Medium),
+    Font(R.font.outfit_semibold, FontWeight.SemiBold)
+)
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = Outfit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Outfit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Outfit,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
